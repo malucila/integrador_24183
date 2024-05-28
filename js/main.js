6bb68ce2bd29596efac9c65bf83b7deaaa7b15e1
Lucila <79485632+malucila@users.noreply.github.com>
2024-05-28 19:22:01 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -22,8 +22,9 @@
         parrafo.innerHTML = "Apellido incorrecto."
         return
     }
-    /* // ANALIZO LA EDAD
+   
     let edadEntera = parseInt(document.formulario.edad.value)
+     
     if(isNaN(edadEntera)){
         // alert("Tiene que ingresar un número valido en la edad.")
         document.formulario.edad.focus()
@@ -37,23 +38,8 @@
             parrafo.innerHTML = "Edad incorrecta."
             return 
         }
-    } */
-    // ANALIZO EL DNI
-   /*  let DNIentero = parseInt(document.formulario.dni.value)
-    if(isNaN(DNIentero)){
-        // alert("Tiene que ingresar un número valido en el DNI.")
-        document.formulario.dni.focus()
-        parrafo.innerHTML = "DNI incorrecto."
-        return
-    }
-    else{
-        if(document.formulario.dni.value.length != 8){
-            // alert("El dni tiene que tener 8 numeros.")
-            document.formulario.dni.focus()
-            parrafo.innerHTML = "DNI incorrecto."
-            return 
-        }
-    } */
+    } 
+   
     // ANALIZO SI SELECCIONÓ DE FORMA CORRECTA
    /*  if(document.formulario.interes.selectedIndex == 0){
         // alert("Debe seleccionar un motivo de su contacto.")
